47b5279142cbe9fdf09b5db7e4a020f82fb8e550
thearavind <thearuvi@gmail.com>
2017-07-26 01:28:36 +0530

    Fetch bill and vote details and save it to db

--- models/bill.go
+++ models/bill.go
@@ -3,14 +3,15 @@ package models
 type Bill struct {
 	BillId                string    `json:"bill_id" bson:"billId"`
 	BillType              string    `json:"bill_type" bson:"billType"`
+	CommitteeCodes        *[]string `json:"committee_codes" bson:"committeeCodes"`
+	SponsorState          string    `json:"sponsor_state" bson:"sponsorState"`
+	SponsorParty          string    `json:"sponsor_party" bson:"sponsorParty"`
+	SponsorUri            string    `json:"sponsor_uri" bson:"sponsorUri"`
+	FacebookAccount       string    `json:"facebook_account" bson:"facebookAccount"`
 	Number                string    `json:"number" bson:"number"`
 	BillUri               string    `json:"bill_uri" bson:"billUri"`
 	Title                 string    `json:"title" bson:"title"`
 	SponsorId             string    `json:"sponsor_id" bson:"sponsorId"`
-	SponsorName           string    `json:"sponsor_name" bson:"sponsorName"`
-	SponsorState          string    `json:"sponsor_state" bson:"sponsorState"`
-	SponsorParty          string    `json:"sponsor_party" bson:"sponsorParty"`
-	SponsorUri            string    `json:"sponsor_uri" bson:"sponsorUri"`
 	GpoPdfUri             string    `json:"gpo_pdf_uri" bson:"gpoPdfUri"`
 	CongressDotGovUrl     string    `json:"congressdotgov_url" bson:"congressDotGovUrl"`
 	GovTrackUrl           string    `json:"govtrack_url" bson:"govTrackUrl"`
@@ -19,12 +20,15 @@ type Bill struct {
 	HousePassage          string    `json:"house_passage" bson:"housePassage"`
 	SenatePassage         string    `json:"senate_passage" bson:"senatePassage"`
 	Enacted               string    `json:"enacted" bson:"enacted"`
-	Vetoed                string    `json:"vetoed" bson:"vetoed"`
+	Vetoed                bool      `json:"vetoed" bson:"vetoed"`
 	Committees            string    `json:"committees" bson:"committees"`
 	PrimarySubject        string    `json:"primary_subject" bson:"primarySubject"`
 	Summary               string    `json:"summary" bson:"summary"`
 	SummaryShort          string    `json:"summary_short" bson:"summaryShort"`
 	LatestMajorActionDate string    `json:"latest_major_action_date" bson:"latestMajorActionDate"`
 	LatestMajorAction     string    `json:"latest_major_action" bson:"latestMajorAction"`
-	CommitteeCodes        *[]string `json:"committee_codes" bson:"committeeCodes"`
+	Votes                 []Vote    `json:"vote" bson:"votes"`
+	VotesUrl              []struct {
+		APIURL string `json:"api_url"`
+	} `json:"votes" bson:"-"`
 }
---
+++ models/vote.go
@@ -0,0 +1,52 @@
+package models
+
+type Vote struct {
+	Bill struct {
+		APIURI       string `json:"api_uri" bson:"apiUri"`
+		BillID       string `json:"bill_id" bson:"billId"`
+		LatestAction string `json:"latest_action" bson:"latestAction"`
+		Title        string `json:"title" bson:"title"`
+	} `json:"bill" bson:"bill"`
+	Date       string `json:"date" bson:"date"`
+	Democratic struct {
+		MajorityPosition string `json:"majority_position" bson:"majorityPosition"`
+		No               int64  `json:"no" bson:"no"`
+		NotVoting        int64  `json:"not_voting" bson:"notVoting"`
+		Present          int64  `json:"present" bson:"present"`
+		Yes              int64  `json:"yes" bson:"yes"`
+	} `json:"democratic" bson:"democratic"`
+	Description string `json:"description" bson:"description"`
+	Independent struct {
+		No        int64  `json:"no" bson:"no"`
+		NotVoting string `json:"not_voting" bson:"notVoting"`
+		Present   int64  `json:"present" bson:"present"`
+		Yes       int64  `json:"yes" bson:"yes"`
+	} `json:"independent" bson:"independent"`
+	Positions []struct {
+		DwNominate   float64 `json:"dw_nominate" bson:"dwNominate"`
+		MemberID     string  `json:"member_id" bson:"memberId"`
+		VotePosition string  `json:"vote_position" bson:"votePosition"`
+	} `json:"positions" bson:"positions"`
+	Question   string `json:"question" bson:"question"`
+	Republican struct {
+		MajorityPosition string `json:"majority_position" bson:"majorityPosition"`
+		No               int64  `json:"no" bson:"no"`
+		NotVoting        int64  `json:"not_voting" bson:"notVoting"`
+		Present          int64  `json:"present" bson:"present"`
+		Yes              int64  `json:"yes" bson:"yes"`
+	} `json:"republican" bson:"republican"`
+	Chamber        string `json:"chamber" bson:"chamber"`
+	Result         string `json:"result" bson:"result"`
+	RollCall       int64  `json:"roll_call" bson:"rollCall"`
+	Session        int64  `json:"session" bson:"session"`
+	TieBreaker     string `json:"tie_breaker" bson:"tieBreaker"`
+	TieBreakerVote string `json:"tie_breaker_vote" bson:"tieBreakerVote"`
+	Time           string `json:"time" bson:"time"`
+	Total          struct {
+		No        int64  `json:"no" bson:"no"`
+		NotVoting string `json:"not_voting" bson:"notVoting"`
+		Present   int64  `json:"present" bson:"present"`
+		Yes       int64  `json:"yes" bson:"yes"`
+	} `json:"total" bson:"total"`
+	VoteType string `json:"vote_type" bson:"voteType"`
+}
